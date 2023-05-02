1a50731d5a43a1b1844b0f0af7064212b3ac6f5b
Fábio Santos <fabio3268@gmail.com>
2023-05-02 15:15:44 -0300

    Exemplos

---
+++ javascript/03-estruturas-homogeneas-objetos/exemplo06/assets/scripts.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+const names = [
+    "Fábio",
+    "Maria",
+    "João"
+];
+
+console.log(names);
+
+const employee = {
+   name : "Fábio",
+   lastName : "Santos",
+   age : 47,
+   salary : 5000.00
+};
+
+console.log(employee);
+console.log(employee.name);
+console.log(employee.lastName);
+console.log(employee.salary);
+
