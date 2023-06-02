3ef1c73dcf711d29ed7af7e3fb0836311aefdd11
Fábio Santos <fabio3268@gmail.com>
2023-06-02 15:10:19 -0300

    Correção da Prova

---
+++ javascript/p1-correcao/questao01/readme.md
@@ -0,0 +1,5 @@
+# PWI - P1 - Questão 01
+
+Considerando o vetor de objetos studentsList contendo uma lista de alunos e suas notas:
+1. percorra o vetor exibindo seu conteúdo na tabela id="students-table" (conforme exemplificado no index.html);
+2. no botão id="calculate-average", adicione um evento de click que calcula a média das notas dos alunos, por fim exiba o resultado na div id="average-div";
---
+++ javascript/p1-correcao/questao02/assets/scripts.js
@@ -0,0 +1,57 @@
+const moviesList = [
+    {
+        title: "Matrix",
+        genre: "Ficção",
+        year: 1999,
+        image : "assets/images/matrix.jpg"
+    },
+    {
+        title: "O Poderoso Chefão",
+        genre: "Crime",
+        year: 1972,
+        image : "assets/images/o-poderoso-chefao.jpg"
+    },
+    {
+        title: "Interestelar",
+        genre: "Ficção",
+        year: 2014,
+        image : "assets/images/interestelar.jpg"
+    },
+    {
+        title: "O Senhor dos Anéis: O Retorno do Rei",
+        genre: "Fantasia",
+        year: 2003,
+        image : "assets/images/senhor-dos-aneis.jpg"
+    },
+    {
+        title: "Vingadores: Ultimato",
+        genre: "Ficção",
+        year: 2019,
+        image : "assets/images/vingadores.jpg"
+    },
+    {
+        title: "Cidade de Deus",
+        genre: "Drama",
+        year: 2002,
+        image : "assets/images/cidade-de-deus.jpg"
+    },
+    {
+        title: "O Silêncio dos Inocentes",
+        genre: "Suspense",
+        year: 1991,
+        image : "assets/images/o-silencio-dos-inocentes.jpg"
+    },
+    {
+        title: "Titanic",
+        genre: "Romance",
+        year: 1997,
+        image : "assets/images/titanic.jpg"
+    },
+    {
+        title: "Batman: O Cavaleiro das Trevas",
+        genre: "Ação",
+        year: 2008,
+        image : "assets/images/batman.jpg"
+    }
+];
+
