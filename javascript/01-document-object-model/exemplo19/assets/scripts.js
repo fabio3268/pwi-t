d944eb7b85b81c742186118170997d9223cdf3cc
Fábio Santos <fabio3268@gmail.com>
2023-06-13 16:21:18 -0300

    PDO - Armazenamento Local

--- javascript/01-document-object-model/exemplo19/assets/scripts.js
+++ javascript/01-document-object-model/exemplo19/assets/scripts.js
@@ -2,6 +2,16 @@
 
 
 
+const input = document.querySelector("#text-box");
+const checkbox = document.querySelector("#checkbox");
 
+checkbox.addEventListener("change", () => {
+    //console.log("oi");
+    if(checkbox.checked){
+        input.setAttribute("disabled",true);
+    } else {
+        input.removeAttribute("disabled");
+    }
+});
 
 
