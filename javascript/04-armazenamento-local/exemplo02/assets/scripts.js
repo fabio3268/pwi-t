443f188ae2b040a72936125eecd1780b14e14b37
Fábio Santos <fabio3268@gmail.com>
2023-06-20 07:10:29 -0300

    PDO - Armazenamento Local

--- javascript/04-armazenamento-local/exemplo02/assets/scripts.js
+++ javascript/04-armazenamento-local/exemplo02/assets/scripts.js
@@ -1,3 +1,20 @@
-
-
+const form = document.querySelector("form");
+let users = [];
+const user = {
+    name : "",
+    email : "",
+    password : ""
+};
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if(localStorage.getItem("users")){
+        users = JSON.parse(localStorage.getItem("users"));
+        console.table(users);
+    }
+    user.name = document.querySelector("#name").value;
+    user.email = document.querySelector("#email").value;
+    user.password = document.querySelector("#password").value;
+    users.push(user);
+    localStorage.setItem("users",JSON.stringify(users));
+});
 
