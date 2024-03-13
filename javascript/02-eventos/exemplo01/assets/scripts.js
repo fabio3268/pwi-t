10f4601606c5f7c79553de2a3db6e3b0ff8d08ff
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-03-13 16:41:31 -0300

    Eventos

---
+++ javascript/02-eventos/exemplo01/assets/scripts.js
@@ -0,0 +1,19 @@
+
+
+
+const buttonClick = document.querySelector(".click");
+const buttonReset = document.querySelector(".reset");
+const divCount = document.querySelector("div");
+var count = 0;
+function changeText () {
+    count++;
+    if(count % 2 == 0) {
+        buttonClick.textContent = "Click PAR";
+    } else {
+        buttonClick.textContent = "Click IMPAR";
+    }
+    console.log(count);
+    divCount.textContent = `Contador: ${count}`;
+}
+
+buttonClick.addEventListener("click",changeText);
