4a8da3c5010d418f9146d662536a6d73ca2fda76
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-03-27 15:40:09 -0300

    Estruturas - Exercícios

--- javascript/03-estruturas/exemplo01/assets/scripts.js
+++ javascript/03-estruturas/exemplo01/assets/scripts.js
@@ -2,5 +2,24 @@
 
 
 
-const teams = ["Internacional", "Grêmio", "Juventude", "Caxias", "Brasil de Pelotas"];
+const teams = [
+    "Internacional",
+    "Grêmio",
+    "Juventude",
+    "Caxias",
+    "Brasil de Pelotas",
+    "Ipiranga de Erechim"
+];
 
+console.log(teams);
+
+for(let i = 0; i < teams.length; i++){
+    //console.log(`${i} => ${teams[i]}`);
+    const div = document.createElement("div");
+    div.textContent = teams[i];
+    div.setAttribute("class","teams");
+    console.log(div);
+    document.querySelector("body").insertAdjacentElement(
+        "beforeend",div
+    );
+}
