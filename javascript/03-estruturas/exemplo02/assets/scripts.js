cd07d2efae689c161021901f320c58c3425a1906
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-03-27 16:41:55 -0300

    Estruturas - Exercícios

--- javascript/03-estruturas/exemplo02/assets/scripts.js
+++ javascript/03-estruturas/exemplo02/assets/scripts.js
@@ -4,3 +4,18 @@
 
 const numbers = [1,2,3,4,30,29,289,292,276];
 
+for (let i = 0; i < numbers.length; i++){
+    if(numbers[i] % 2 == 0){
+        const divEven = document.querySelector("#even");
+        const numberEven = document.createElement("div");
+        // setar a clas de numberEven para even
+        numberEven.setAttribute("class","even");
+        // inserir o conteúdo na div
+        numberEven.textContent = numbers[i];
+        // inserir o elemento numberEven em divEven antes do final
+        divEven.insertAdjacentElement("beforeend",numberEven);
+    } else {
+
+
+    }
+}
