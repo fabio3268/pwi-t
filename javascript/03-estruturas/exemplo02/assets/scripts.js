f468363c04ab71b245abc28169d8ff2411ebb614
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-04-02 16:03:14 -0300

    Estruturas - Exercícios - Exemplos

--- javascript/03-estruturas/exemplo02/assets/scripts.js
+++ javascript/03-estruturas/exemplo02/assets/scripts.js
@@ -4,18 +4,38 @@
 
 const numbers = [1,2,3,4,30,29,289,292,276];
 
-for (let i = 0; i < numbers.length; i++){
-    if(numbers[i] % 2 == 0){
-        const divEven = document.querySelector("#even");
-        const numberEven = document.createElement("div");
-        // setar a clas de numberEven para even
-        numberEven.setAttribute("class","even");
-        // inserir o conteúdo na div
-        numberEven.textContent = numbers[i];
-        // inserir o elemento numberEven em divEven antes do final
-        divEven.insertAdjacentElement("beforeend",numberEven);
+const divEven = document.querySelector("#even");
+const divOdd = document.querySelector("#odd");
+
+numbers.forEach((e) => {
+    const divNumber = document.createElement("div");
+    divNumber.textContent = e;
+    if(e % 2 == 0) {
+        divNumber.setAttribute("class","even");
+        divEven.appendChild(divNumber);
     } else {
-
-
+        divNumber.setAttribute("class","odd");
+        divOdd.appendChild(divNumber);
     }
-}
+});
+
+/*
+for (let i = 0; i < numbers.length; i++){
+    const divNumber = document.createElement("div");
+    divNumber.textContent = numbers[i];
+    if(numbers[i] % 2 == 0){
+        divNumber.setAttribute("class","even");
+        divEven.appendChild(divNumber);
+    } else {
+        divNumber.setAttribute("class","odd");
+        divOdd.appendChild(divNumber);
+    }
+}*/
+
+
+console.table(numbers);
+numbers.forEach((e,i,arr) => {
+    console.log(`${i} - ${e}`);
+    arr[i] = arr[i] + 2;
+});
+console.table(numbers);
