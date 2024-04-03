1a0199f6d26073b94842c863ca2caa1e806e0f39
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-04-03 15:43:11 -0300

    Estruturas - Exercícios

--- javascript/03-estruturas/exemplo03/assets/scripts.js
+++ javascript/03-estruturas/exemplo03/assets/scripts.js
@@ -1,6 +1,6 @@
 
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const tbody = document.querySelector("#tableBody");
 numbers.forEach((value,i) => {
     const row = document.createElement("tr");
@@ -11,14 +11,17 @@ numbers.forEach((value,i) => {
 const searchButton = document.querySelector("#searchButton");
 const searchInput = document.querySelector("#searchInput");
 const message = document.querySelector(".message");
-searchButton.addEventListener("click",() => {
-    let resultSearch = numbers.indexOf(parseInt(searchInput.value))
-    if(resultSearch == -1) {
-        message.innerHTML = "Número não encontrado";
+
+searchButton.addEventListener("click", () => {
+    console.log("Vou buscar:" + searchInput.value);
+    var searchValue = parseInt(searchInput.value);
+    var result = numbers.indexOf(searchValue);
+    if(result == -1){
+        message.textContent = "Número não encontrado";
     } else {
-        message.innerHTML = `Número encontrado na posição ${resultSearch}`;
+        message.textContent = `Número encontrado na posição ${result}`;
     }
 });
 
-
+//console.log(numbers.indexOf(30));
 
