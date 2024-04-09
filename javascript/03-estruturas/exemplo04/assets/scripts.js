4c7d5293ddd0da53d845b05e02ab36285744709e
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-04-09 15:24:13 -0300

    Estruturas - Exercícios

--- javascript/03-estruturas/exemplo04/assets/scripts.js
+++ javascript/03-estruturas/exemplo04/assets/scripts.js
@@ -7,10 +7,20 @@ var report = [
     [9.8,9,8.5]
 ];
 
+console.log(report);
+
+report[0][0] = 10;
+console.log(report[0][0]); // L C
+report[0][1] = 10;
+console.log(report[0][1]);
+
+console.table(report);
+
+
 //report[1][2] = 10;
 
 //console.log(report[1][2]);
-console.table(report);
+//console.table(report);
 
 /*for(let c = 0; c <= 2; c++){
     console.log(report[0][c]);
