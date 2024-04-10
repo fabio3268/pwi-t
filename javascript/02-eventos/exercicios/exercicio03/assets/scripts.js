79596d21c55d14850a0476c681a420a0414a7fdf
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-04-10 15:09:49 -0300

    Estruturas - Exercícios

--- javascript/02-eventos/exercicios/exercicio03/assets/scripts.js
+++ javascript/02-eventos/exercicios/exercicio03/assets/scripts.js
@@ -3,10 +3,19 @@
 const myList = document.querySelector("#myList");
 const inputItem = document.querySelector("#item");
 
-
-
 document.querySelector("#myButtonInsert").addEventListener("click", () => {
     const newItem = document.createElement("li");
     newItem.innerHTML = `${inputItem.value}<button class="delete">Excluir</button>`;
     myList.appendChild(newItem);
 });
+
+myList.addEventListener("click", (event) => {
+    console.log(event.target.tagName);
+    if(event.target.tagName == "BUTTON"){
+        console.log("Apagar um item");
+        console.log();
+        event.target.parentElement.remove();
+    }
+});
+
+
