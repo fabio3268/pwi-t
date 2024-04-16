9eb945a5bac34c8faa9ce201f0060034430ffec8
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-04-16 15:10:11 -0300

    Estruturas - Exercícios

--- javascript/03-estruturas/exemplo05/assets/scripts.js
+++ javascript/03-estruturas/exemplo05/assets/scripts.js
@@ -3,12 +3,4 @@
 
 
 
-const myArray = [1,2,3,4,30,29];
-console.table(myArray);
-function show (e, i, array){
-    //console.log("Olá, Mundo!");
-    console.log(i, e);
-    console.table(array);
-}
-//show();
-myArray.forEach(show);
+
