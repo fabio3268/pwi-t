de6a7b148a051ed947ec446b23f4fb23a8794e97
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-07-03 16:38:06 -0300

    Estruturas - Objetos - Exercícios

--- javascript/03-estruturas/exercicios/exercicio06/assets/scripts.js
+++ javascript/03-estruturas/exercicios/exercicio06/assets/scripts.js
@@ -1,2 +1,29 @@
 
 
+let studentsArray = [];
+
+document.querySelector("#submit").addEventListener("click",() => {
+    let student = {
+        name: document.querySelector("#name").value,
+        subject : document.querySelector("#subject").value,
+        grade : document.querySelector("#grade").value
+    };
+    //console.log("Oi");
+    //console.log(student);
+    studentsArray.push(student);
+    console.log(studentsArray);
+    const rowStudent = document.createElement("tr");
+    rowStudent.innerHTML = `<td>${student.name}</td><td>${student.subject}</td><td>${student.grade}</td><td><button type="button">Excluir</button></td>`;
+    document.querySelector("tbody").appendChild(rowStudent);
+});
+
+document.querySelector("tbody").addEventListener("click", (event) => {
+    if(event.target.tagName = "BUTTON"){
+        // remover da lista de objetos
+        // pegar o conteúdo da segunda célula da TR
+        const studentName = event.target.parentElement.parentElement.children[0].textContent;
+        // remover o estudante contido no studantNAme do array studentsArray com a função splice
+        event.target.parentElement.parentElement.remove();
+        console.log(studentsArray);
+    }
+});
