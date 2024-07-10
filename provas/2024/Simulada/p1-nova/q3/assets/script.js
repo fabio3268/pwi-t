783a0a4fa7c88195ee1f3f8293b3f2643aee444b
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-07-10 16:26:11 -0300

    Prova Simulada

--- provas/2024/Simulada/p1-nova/q3/assets/script.js
+++ provas/2024/Simulada/p1-nova/q3/assets/script.js
@@ -1,2 +1,30 @@
 let studentsList = [];
 
+const student = {
+    name: "",
+    age: ""
+};
+
+const buttonAdd = document.querySelector("#add");
+const buttonOldest = document.querySelector("#oldest");
+buttonAdd.addEventListener("click", () => {
+    const student = {
+        name: document.querySelector("#name").value,
+        age: document.querySelector("#age").value
+    };
+    studentsList.push(student);
+    console.log(studentsList);
+});
+
+buttonOldest.addEventListener("click", () => {
+    console.log("Old");
+    let ageOldest = 0;
+    let indexOldest = -1;
+    studentsList.forEach((element,index) => {
+        if(Number(element.age) > Number(ageOldest)) {
+            ageOldest = element.age;
+            console.log(ageOldest);
+            document.querySelector("#result").textContent = element.name;
+        }
+    });
+});
