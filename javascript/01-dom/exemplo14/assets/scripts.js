8d7deaab7eb63d81f23b737f93032e3d94c1e2f9
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-07-23 16:43:51 -0300

    Manipulando DOM - Parte 02

--- javascript/01-dom/exemplo14/assets/scripts.js
+++ javascript/01-dom/exemplo14/assets/scripts.js
@@ -1,6 +1,22 @@
 const plans = [
-    { name: 'Básico', price: 0, description: 'Funcionalidades básicas' },
+    {
+        name: 'Básico',
+        price: 0,
+        description: 'Funcionalidades básicas'
+    },
     { name: 'Pessoal', price: 9.99, description: 'Básico + Ferramentas avançadas' },
     { name: 'Profissional', price: 19.99, description: 'Pessoal + Suporte dedicado' },
     { name: 'Empresarial', price: 39.99, description: 'Pro + Armazenamento ilimitado' },
 ];
+
+const divList = document.querySelectorAll(".box");
+console.log(divList);
+divList.forEach((element,index) => {
+    console.log(index);
+    element.innerHTML = `
+        <div class="name">${plans[index].name}</div>
+        <div class="price">${plans[index].price}</div>
+        <div class="description">${plans[index].description}</div>
+    `;
+});
+
