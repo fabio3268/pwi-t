fda6366941fed973c4f575115f2ae95a7a3ac318
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-08-14 16:30:35 -0300

    Armazenamento Local

--- javascript/04-armazenamento-local/exemplo02/assets/get-users-scripts.js
+++ javascript/04-armazenamento-local/exemplo02/assets/get-users-scripts.js
@@ -2,25 +2,30 @@
 
 
 const usersList = JSON.parse(localStorage.getItem("usersList"));
-
 const listUsersContainer = document.querySelector(".container");
 
-usersList.forEach((user, index) => {
-    const userDiv = document.createElement("div");
-    userDiv.classList.add("user");
-    userDiv.innerHTML = `
+function showListUsers () {
+    listUsersContainer.innerHTML = "";
+    usersList.forEach((user, index) => {
+        const userDiv = document.createElement("div");
+        userDiv.classList.add("user");
+        userDiv.innerHTML = `
         <span class="name">${user.name}</span>
         <span class="email">${user.email}</span>
         <span class="remove"><button userIndex="${index}">X</button></span>
         `;
-    listUsersContainer.appendChild(userDiv);
-});
+        listUsersContainer.appendChild(userDiv);
+    });
+}
 
-document.querySelectorAll(".remove").forEach((element) => {
+showListUsers();
+
+document.querySelectorAll("button").forEach((element) => {
     element.addEventListener("click", (event) => {
         console.log(`Remover: ${event.target.getAttribute("userIndex")}` );
-        // remover uma posição específica da listUsers
+            // remover uma posição específica da listUsers
         usersList.splice(event.target.getAttribute("userIndex"), 1);
         console.log(usersList);
+        showListUsers();
     })
 });
--- javascript/04-armazenamento-local/exemplo02/assets/scripts.js
+++ javascript/04-armazenamento-local/exemplo02/assets/scripts.js
@@ -1,6 +1,6 @@
 
 
-localStorage.removeItem("usersList");
+//localStorage.removeItem("usersList");
 
 document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
